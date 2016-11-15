1caf84b57ba29fadbfbfb05d0ec7fef4d49fd9f5
ToxaZhe <muroslava.m.2012@gmail.com>
2016-11-16 00:05:26 +0200

    add checking for cell at index

--- Homework/ViewController.m
+++ Homework/ViewController.m
@@ -22,6 +22,7 @@
 @property(nonatomic, strong) CoraDataManager* dataManager;
 @property(nonatomic, strong) NSMutableArray* dowloadManagerCollection;
 @property(nonatomic, strong) NSMutableDictionary* completedDownloadsDates;
+@property(nonatomic, strong) NSMutableSet *cellIndexes;
 
 @property BOOL finished;
 
@@ -38,6 +39,7 @@
     [nav setNavigationBarHidden:YES animated:YES];
     [self.tableView setSeparatorColor:[UIColor cyanColor]];
     _completedDownloadsDates = [[NSMutableDictionary alloc] init];
+    _cellIndexes = [NSMutableSet set];
     [self initAndCleanCoreData];
     [self fillArrayWithUrlStrings];
 }
@@ -83,17 +85,28 @@
 //        NSInteger completedDownloadsNumber = [fetchedCoreDataString count];
         if (cell.moved) {
           [self.completedDownloadsDates setObject:[fetchedCoreDataString lastObject] forKey:[NSString stringWithFormat:@"%ld", (long)indexPath.row]];
+            [self.cellIndexes addObject: [NSNumber numberWithInteger:index]];
+            cell.moved = NO;
         }
         
 //        NSInteger finishedNumber = completedDownloadsNumber - 1;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            cell.progresLabel.text = [self.completedDownloadsDates objectForKey:[NSString stringWithFormat:@"%ld", (long)indexPath.row]];
-            NSLog(@"%@", [self.completedDownloadsDates objectForKey:[NSString stringWithFormat:@"%@", indexPath]]);
-            cell.nameLabel.text = @"Push to listen";
-            NSLog(@"%@", self.completedDownloadsDates);
+        
+        if ([_cellIndexes containsObject: [NSNumber numberWithInteger:index]]) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                cell.progresLabel.text = [self.completedDownloadsDates objectForKey:[NSString stringWithFormat:@"%ld", (long)indexPath.row]];
+                NSLog(@"%@", [self.completedDownloadsDates objectForKey:[NSString stringWithFormat:@"%@", indexPath]]);
+                cell.nameLabel.text = @"Push to listen";
+                NSLog(@"%@", self.completedDownloadsDates);
+            });
+        } else {
+            cell.nameLabel.text = [NSString stringWithFormat:@"Push to dowload Song %li", index + 1];
+            NSString* progress = [NSString stringWithFormat:@"%.2f", (double)cell.download.bigFileData.length / (double)cell.download.expectedBigFileLength * 100.0];
+            cell.progresLabel.text = progress;
+        }
+        
 //            NSLog(@"finished number - %li against indexPath.row - %li", finishedNumber, indexPath.row);
-            cell.moved = NO;
-        });
+            
+        
         if (fetchedCoreDataString.count == _urlStrings.count){
             [_t invalidate];
         }
@@ -125,9 +138,17 @@
     NSString* song5UrlString = @"https://promodj.com/download/5920607/Kristina%20Schtotz%20-%20Like%20A%20Bird%20%28A-Mase%20Chill-Out%20Version%29%20%28promodj.com%29.mp3";
      NSString* song6UrlString = @"http://promodj.com/download/4828023/Da%20Rave%20-%20Milky%20Way%20%28Original%20Mix%29%20%28promodj.com%29.mp3";
      NSString* song7UrlString = @"http://promodj.com/download/1746719/%D0%AF%D0%B6%D0%B5%D0%92%D0%B8%D0%BA%D0%B0%20%E2%80%94%20%D0%9B%D0%B5%D1%82%D0%BE%20%28original%29%20%28promodj.com%29.mp3";
+    NSString* song8UrlString = @"http://promodj.com/download/5658805/SNV%20Production%20-%20Alone%20%28promodj.com%29.mp3";
+    NSString* song9UrlString = @"http://promodj.com/download/439301/Implex%20-%20The%20Shape%20of%20Time%20%28promodj.com%29.mp3";
+    NSString* song10UrlString = @"http://promodj.com/download/770564/Trance%20Brothers%20Eternal%20Life%20%28promodj.com%29.mp3";
+    NSString* song11UrlString = @"http://promodj.com/download/5491668/AN.DU%20-%20Africanizm%20%28Original%20Mix%29%20%28promodj.com%29.mp3";
+    NSString* song12UrlString = @"http://promodj.com/download/2903865/Sac%20Noel%20vs%20DJ.Steel%20%28%20%D0%9A%D0%B5%D0%BC%D0%B5%D1%80%D0%BE%D0%B2%D0%BE%29%20%28promodj.com%29.mp3";
+    NSString* song13UrlString = @"http://promodj.com/download/723082/KLUBBDANCE%20DJS%20-%20Poky%20Beats%20%28Original%20Mix%29%20%28promodj.com%29.mp3";
+    NSString* song14UrlString = @"http://promodj.com/download/532922/EXIT%20project%20%3A%3A%20Shanti%20Place%20-%20%20%27Opium-v.2%27%20%28promodj.com%29.mp3";
+    NSString* song15UrlString = @"http://promodj.com/download/2043067/DJ%20Schmolli%20-%20Rock%20Wit%20The%20Original%20Waterpumpee%20%28Mashup%29%20%28promodj.com%29.mp3";
+    NSString* song16UrlString = @"http://promodj.com/download/5019030/Timian%2C%20Rusalina%20feat.%20Syntheticsax%20-%20I%27m%20falling%20for%20you%20%28Original%20mix%29%20%28promodj.com%29.mp3";
     
-    
-    self.urlStrings = [[NSArray alloc] initWithObjects:song1UrlString, song2UrlString,song3UrlString, song4UrlString, song5UrlString,song6UrlString, song7UrlString,  nil];
+    self.urlStrings = [[NSArray alloc] initWithObjects:song1UrlString, song2UrlString,song3UrlString, song4UrlString, song5UrlString,song6UrlString, song7UrlString, song8UrlString, song9UrlString, song10UrlString, song11UrlString, song12UrlString, song13UrlString, song14UrlString, song15UrlString, song16UrlString,  nil];
     [self setDownloadsForDownloadManager:_urlStrings];
 }
 
